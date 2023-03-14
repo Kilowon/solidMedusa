8a3591bab8e9d6c20449c68b4d15cc45e376e653
Kilowon <shauncameron.inbox@gmail.com>
2023-03-14 11:29:24 -0400

    updating Meta info for SEO

--- src/root.tsx
+++ src/root.tsx
@@ -10,7 +10,8 @@ import {
 	Meta,
 	Routes,
 	Scripts,
-	Title
+	Title,
+	Link
 } from 'solid-start'
 import 'uno.css'
 import '@unocss/reset/tailwind.css'
@@ -24,6 +25,18 @@ export default function Root() {
 				<Title>SolidStart - Bare</Title>
 				<Meta charset="utf-8" />
 				<Meta name="viewport" content="width=device-width, initial-scale=1" />
+				<Meta name="description" content="SolidJs-MedusaJs-Starter" />
+				<Link rel="icon" href="/favicon.ico" />
+				<Link
+					rel="preload"
+					as="image"
+					href="https://res.cloudinary.com/contentdelivery/image/upload/c_scale,w_600,z_1/v1677462156/hero_b4p7fs.webp"
+				/>
+				<Link
+					rel="preload"
+					as="image"
+					href="https://res.cloudinary.com/contentdelivery/image/upload/c_scale,w_440/v1677462424/cta_three_voqqzi.webp"
+				/>
 			</Head>
 			<Body>
 				<GlobalContextProvider>

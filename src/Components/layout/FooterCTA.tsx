5139ecf8d4a6e6a5334a0f264fe81460e480d2c8
Kilowon <shauncameron.inbox@gmail.com>
2023-03-17 15:23:40 -0400

    Fix: image not showing

--- src/Components/layout/FooterCTA.tsx
+++ src/Components/layout/FooterCTA.tsx
@@ -1,10 +1,15 @@
-import { Switch, Match } from 'solid-js'
+import { Switch, Match, createEffect, createSignal } from 'solid-js'
 import { useWindowSize } from '@solid-primitives/resize-observer'
 import UnderlineLink from '~/Components/common/Underline-link'
+import { getWindowSize } from '@solid-primitives/resize-observer'
 
-const size = useWindowSize()
+const [size, setSize] = createSignal(getWindowSize())
 
 export function FooterCTA() {
+	createEffect(() => {
+		setSize(getWindowSize())
+		size()
+	})
 	return (
 		<div class="bg-amber-100 w-full">
 			<div class="max-w-[1440px] w-full mx-auto px8 flex flex-col-reverse gap-y-8 sm:flex-row sm:items-center justify-between py-16 relative">
@@ -16,18 +21,18 @@ export function FooterCTA() {
 				</div>
 				<div class="relative w-full aspect-square sm:w-[35%] sm:aspect-[28/36]">
 					<Switch fallback={<div>Loading...</div>}>
-						<Match when={size.width < 400}>
+						<Match when={size().width < 400}>
 							<img
 								src="https://res.cloudinary.com/contentdelivery/image/upload/c_scale,w_440/v1677462424/cta_three_voqqzi.webp"
 								alt=""
-								class="absolute inset-0 w-full h-full object-cover sm:invisible"
+								class="absolute inset-0 w-full h-full object-cover "
 							/>
 						</Match>
-						<Match when={size.width >= 400}>
+						<Match when={size()?.width > 400}>
 							<img
 								src="https://res.cloudinary.com/contentdelivery/image/upload/c_scale,w_840/v1677462424/cta_three_voqqzi.webp"
 								alt=""
-								class="absolute inset-0 w-full h-full object-cover invisible sm:visible"
+								class="absolute inset-0 w-full h-full object-cover "
 							/>
 						</Match>
 					</Switch>

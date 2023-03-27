766ccb8c33aa078fb0fdc1e79bfc66cf6aff6544
Kilowon <shauncameron.inbox@gmail.com>
2023-03-27 08:46:13 -0400

    Added Show module to control undefined

--- src/Components/ProductTemplate.tsx
+++ src/Components/ProductTemplate.tsx
@@ -4,7 +4,7 @@ import { createRouteData, ServerError, refetchRouteData } from 'solid-start'
 import { getProductInfo, IsClientCheck } from '~/Services/medusaAPI'
 import { useGlobalContext } from '~/Context/Providers'
 import { createEffect, onMount } from 'solid-js'
-import { JSX } from 'solid-js'
+import { JSX, Show } from 'solid-js'
 import { Product } from '~/types/medusa'
 import { title } from 'process'
 
@@ -13,27 +13,29 @@ export default function ProductTemplate(props: {
 	productInfo: Product
 }): JSX.Element {
 	return (
-		<main>
-			<div class="content-container flex flex-col sm:flex-row sm:items-start py-6 relative">
-				<div class="flex flex-col gap-y-8 w-full">
-					<ImageGallery images={props.images} />
-				</div>
-				<div class="sm:sticky sm:top-20 w-full py-8 sm:py-0 sm:max-w-[344px] md:max-w-[400px] flex flex-col gap-y-12">
-					<div class="flex flex-col gap-y-12 lg:max-w-[500px] mx-auto">
-						<div>
-							<ProductActions productInfo={props.productInfo} />
-						</div>
+		<Show when={props.productInfo}>
+			<main>
+				<div class="content-container flex flex-col sm:flex-row sm:items-start py-6 relative">
+					<div class="flex flex-col gap-y-8 w-full">
+						<ImageGallery images={props.images} />
 					</div>
-					{/* <ProductTabs productId={product} /> */}
+					<div class="sm:sticky sm:top-20 w-full py-8 sm:py-0 sm:max-w-[344px] md:max-w-[400px] flex flex-col gap-y-12">
+						<div class="flex flex-col gap-y-12 lg:max-w-[500px] mx-auto">
+							<div>
+								<ProductActions productInfo={props.productInfo} />
+							</div>
+						</div>
+						{/* <ProductTabs productId={product} /> */}
+					</div>
 				</div>
-			</div>
-			<div class="content-container my-16 px-6 sm:px-8 sm:my-32">
-				{/* <RelatedProducts product={product} /> */}
-			</div>
-			{/* <MobileActions
+				<div class="content-container my-16 px-6 sm:px-8 sm:my-32">
+					{/* <RelatedProducts product={product} /> */}
+				</div>
+				{/* <MobileActions
 			product={product}
 			show={!inView}
 		/> */}
-		</main>
+			</main>
+		</Show>
 	)
 }
--- src/Components/prod_tp_components/ProductActions.tsx
+++ src/Components/prod_tp_components/ProductActions.tsx
@@ -1,4 +1,4 @@
-import { JSX, For } from 'solid-js'
+import { JSX, For, Show } from 'solid-js'
 import { A } from 'solid-start'
 import { Product } from '~/types/medusa'
 import clsx from 'clsx'
@@ -20,34 +20,35 @@ export default function ProductActions(props: {
 	}, [price]) */
 	console.log('ProductActions props', props.productInfo?.collection.id)
 	return (
-		<div class="flex flex-col gap-y-2">
-			<A
-				href={`/collections/${props.productInfo?.collection.id}`}
-				class="text-sm text-gray-700"
-			>
-				{props.productInfo?.collection.title}
-			</A>
+		<Show when={props.productInfo}>
+			<div class="flex flex-col gap-y-2">
+				<A
+					href={`/collections/${props.productInfo?.collection.id}`}
+					class="text-sm text-gray-700"
+				>
+					{props.productInfo?.collection.title}
+				</A>
 
-			<h3 class="text-xl">{props.productInfo?.title}</h3>
+				<h3 class="text-xl">{props.productInfo?.title}</h3>
 
-			<p class="text-base">{props.productInfo?.description}</p>
+				<p class="text-base">{props.productInfo?.description}</p>
 
-			<div class="my-8 flex flex-col gap-y-6">
-				<For each={props.productInfo?.options}>
-					{option => {
-						return (
-							<div>
-								<OptionSelect
+				{props.productInfo?.variants.length > 1 && (
+					<div class="my-8 flex flex-col gap-y-6">
+						{props.productInfo?.options.map(option => {
+							return (
+								<div>
+									{/* <OptionSelect
 									option={option}
 									title={option.title}
-								/>
-							</div>
-						)
-					}}
-				</For>
-			</div>
+								/> */}
+								</div>
+							)
+						})}
+					</div>
+				)}
 
-			{/* <div class="mb-4">
+				{/* <div class="mb-4">
 				{selectedPrice ? (
 					<div class="flex flex-col text-gray-700">
 						<span
@@ -72,9 +73,10 @@ export default function ProductActions(props: {
 				)}
 			</div> */}
 
-			<button class="w-full uppercase flex items-center justify-center min-h-[50px] px-5 py-[10px] text-sm border transition-colors duration-200 disabled:opacity-50 text-white bg-gray-900 border-gray-900 hover:bg-white hover:text-gray-900 disabled:hover:bg-gray-900 disabled:hover:text-white">
-				Add to cart
-			</button>
-		</div>
+				<button class="w-full uppercase flex items-center justify-center min-h-[50px] px-5 py-[10px] text-sm border transition-colors duration-200 disabled:opacity-50 text-white bg-gray-900 border-gray-900 hover:bg-white hover:text-gray-900 disabled:hover:bg-gray-900 disabled:hover:text-white">
+					Add to cart
+				</button>
+			</div>
+		</Show>
 	)
 }

3950b3708de627e3e4a70cf4a5787fa11a6a3002
Kilowon <shauncameron.inbox@gmail.com>
2023-04-21 19:31:04 -0400

    Moved Slider Component to own file

--- src/routes/(app)/categories/[handle].tsx
+++ src/routes/(app)/categories/[handle].tsx
@@ -1,15 +1,13 @@
 import { useGlobalContext } from '~/Context/Providers'
 import { useParams, Title, Meta, A } from 'solid-start'
-import { createEffect, createSignal, For, Show } from 'solid-js'
-import ProductPreview from '~/Components/nav_components/ProductPreview'
+import { createEffect, createSignal, For, Show, Switch, Match } from 'solid-js'
 import 'solid-slider/slider.css'
-import { Slider, createSlider, SliderProvider } from 'solid-slider'
+import { TransitionGroup, Transition } from 'solid-transition-group'
 import {
-	Arrow,
-	SliderDots,
-	SliderArrowLeft,
-	SliderArrowRight
-} from '~/Components/common/SliderExtended'
+	SingleLineSlider,
+	DoubleLineSlider,
+	TripleLineSlider
+} from '~/Components/common/ProductSlider'
 
 export default function Categories() {
 	const params = useParams()
@@ -19,6 +17,7 @@ export default function Categories() {
 	const [currentSlide, setCurrentSlide] = createSignal(0)
 	const [loaded, setLoaded] = createSignal(false)
 	const [dots, setDots] = createSignal()
+	const [parentCategories, setParentCategories] = createSignal([])
 
 	function filterCategories() {
 		return categories()?.filter(
@@ -35,9 +34,32 @@ export default function Categories() {
 
 	createEffect(() => {
 		if (categoryProducts()?.length === 0) {
-			setDots(0)
+			setDots(Number(0))
 		}
 	})
+
+	function getParentCategories(categories: any[], params: any) {
+		const parentCategories = [
+			categories.find((category: any) => category.handle === params.handle)
+		]
+
+		while (parentCategories[parentCategories.length - 1]?.parent_category) {
+			const parentCategory = categories.find(
+				(category: any) =>
+					category.name ===
+					parentCategories[parentCategories.length - 1]?.parent_category?.name
+			)
+			parentCategories.push(parentCategory)
+		}
+
+		setParentCategories(parentCategories.reverse() as never[])
+
+		return parentCategories
+	}
+
+	createEffect(() => {
+		getParentCategories(categories(), params)
+	}, [currentCategory()])
 
 	return (
 		<div>
@@ -57,121 +79,177 @@ export default function Categories() {
 						<div class="flex flex-col  mb-16 ">
 							<span class="text-base text-gray-600 mb-6">Shop By Category</span>
 							<div class="text-2xl text-gray-900 ">
-								<ol class="flex flex-row ">
-									<Show when={currentCategory()[0]?.parent_category?.name}>
-										<li class="pr-3">
-											<A
-												href={`/categories/${
-													currentCategory()[0]?.parent_category?.handle
-												}`}
-											>
-												{currentCategory()[0]?.parent_category?.name}
-											</A>
-										</li>
-										<div class="pr-3 font-bold text-3xl flex flex-row items-center ">
-											<div class={'i-ic-outline-chevron-right'} />
-										</div>
-									</Show>
-									<Show when={currentCategory()[0]?.name}>
-										<li class="pr-3">
-											<A href={`/categories/${currentCategory()[0]?.handle}`}>
-												<div class="text-amber-5">{currentCategory()[0]?.name}</div>
-											</A>
-										</li>
-										<div class="pr-3 font-bold text-3xl flex flex-row items-center ">
-											<div
-												class={
-													currentCategory()[0]?.category_children.length === 0
-														? ''
-														: 'i-ic-outline-chevron-right'
-												}
-											/>
-										</div>
-									</Show>
-									<Show when={currentCategory()[0]?.category_children}>
-										<For each={currentCategory()[0]?.category_children}>
-											{(child: any, index) => (
-												<div class="flex flex-row">
-													<li class="pr-3">
-														<A href={`/categories/${child?.handle}`}>{child?.name}</A>
-													</li>
-													<div class="flex flex-row items-center text-xl pr-3  ">
+								<ol class="flex flex-row flex-wrap">
+									<Show when={parentCategories()}>
+										<TransitionGroup
+											onEnter={(el, done) => {
+												const a = el.animate([{ opacity: 0 }, { opacity: 1 }], {
+													duration: 250
+												})
+												a.finished.then(done)
+											}}
+											onExit={(el, done) => {
+												const a = el.animate([{ opacity: 1 }, { opacity: 0 }], {
+													duration: 0
+												})
+												a.finished.then(done)
+											}}
+										>
+											<For each={parentCategories()}>
+												{(category: any) => (
+													<li class="flex flex-row pr-1 whitespace-nowrap">
+														<A href={`/categories/${category.handle}`}>
+															<div
+																class={
+																	category.name === currentCategory()[0]?.name
+																		? 'text-amber-5'
+																		: ''
+																}
+															>
+																{category.name}
+															</div>
+														</A>
 														<div
-															class={
-																currentCategory()[0]?.category_children.length === index() + 1
-																	? ' '
-																	: 'i-ph-dot-outline-light'
-															}
-														/>
+															class={'pr-1 font-bold text-3xl flex flex-row items-center '}
+														>
+															<div
+																class={
+																	category.category_children.length === 0
+																		? ''
+																		: 'i-ic-outline-chevron-right'
+																}
+															/>
+														</div>
+													</li>
+												)}
+											</For>
+										</TransitionGroup>
+									</Show>
+
+									<Show when={currentCategory()[0]?.category_children}>
+										<TransitionGroup
+											onEnter={(el, done) => {
+												const a = el.animate([{ opacity: 0 }, { opacity: 1 }], {
+													duration: 250
+												})
+												a.finished.then(done)
+											}}
+											onExit={(el, done) => {
+												const a = el.animate([{ opacity: 1 }, { opacity: 0 }], {
+													duration: 0
+												})
+												a.finished.then(done)
+											}}
+										>
+											<For each={currentCategory()[0]?.category_children}>
+												{(child: any, index) => (
+													<div class="flex flex-row whitespace-nowrap">
+														<li class="pr-1">
+															<A href={`/categories/${child?.handle}`}>{child?.name}</A>
+														</li>
+														<div class="flex flex-row items-center text-xl pr-1   ">
+															<div
+																class={
+																	currentCategory()[0]?.category_children.length === index() + 1
+																		? ' '
+																		: 'i-ph-dot-outline-light'
+																}
+															/>
+														</div>
 													</div>
-												</div>
-											)}
-										</For>
+												)}
+											</For>
+										</TransitionGroup>
 									</Show>
 								</ol>
 							</div>
 						</div>
-						<SliderProvider>
-							<ul class="flex flex-row relative">
-								<Show when={categoryProducts()?.length > 0}>
-									<Slider
-										options={{
-											loop: true,
-											slides: {
-												perView: 3,
-												spacing: 15
-											},
-											initial: 0,
-											slideChanged(slider) {
-												setCurrentSlide(slider.track.details.rel)
-											},
-											created(slider) {
-												setLoaded(true)
-												setDots(() => {
-													console.log('SLIDER', slider.track.details.slides?.length)
 
-													return [...Array(slider.track.details.slides?.length).keys()]
-												})
-											},
-											optionsChanged(slider) {
-												setDots(() => {
-													console.log('SLIDER', slider.track.details.slides?.length)
+						<Transition
+							onEnter={(el, done) => {
+								const a = el.animate([{ opacity: 0 }, { opacity: 1 }], {
+									duration: 250
+								})
+								a.finished.then(done)
+							}}
+							onExit={(el, done) => {
+								const a = el.animate([{ opacity: 1 }, { opacity: 0 }], {
+									duration: 0
+								})
+								a.finished.then(done)
+							}}
+						>
+							<Show when={categoryProducts()?.length <= 6}>
+								<SingleLineSlider
+									slideVisible={3}
+									categoryProducts={categoryProducts}
+									setCurrentSlide={setCurrentSlide}
+									setLoaded={setLoaded}
+									setDots={setDots}
+									loaded={loaded}
+									currentSlide={currentSlide}
+									dots={dots}
+								/>
+							</Show>
+						</Transition>
 
-													return [...Array(slider.track.details.slides?.length).keys()]
-												})
-											}
-										}}
-									>
-										<For each={categoryProducts()}>
-											{product => (
-												<li>
-													<ProductPreview {...product} />
-												</li>
-											)}
-										</For>
-									</Slider>
-									<Show when={loaded()}>
-										<SliderArrowLeft
-											prev
-											class={
-												'w-10 h-10 absolute left-0 top-[50%] fill-coolGray-8 hover:fill-amber-500'
-											}
-										/>
-										<SliderArrowRight
-											next
-											class={
-												'w-10 h-10 absolute right-0 top-[50%] fill-coolGray-8 hover:fill-amber-500'
-											}
-										/>
-									</Show>
-								</Show>
-							</ul>{' '}
-							<SliderDots
-								dots={dots()}
-								currentSlide={currentSlide()}
-								class={' w-[10px] h-[10px] rounded-full'}
-							/>
-						</SliderProvider>
+						<Transition
+							onEnter={(el, done) => {
+								const a = el.animate([{ opacity: 0 }, { opacity: 1 }], {
+									duration: 250
+								})
+								a.finished.then(done)
+							}}
+							onExit={(el, done) => {
+								const a = el.animate([{ opacity: 1 }, { opacity: 0 }], {
+									duration: 0
+								})
+								a.finished.then(done)
+							}}
+						>
+							<Show
+								when={categoryProducts()?.length > 6 && categoryProducts()?.length < 10}
+							>
+								<DoubleLineSlider
+									slideVisible={3}
+									categoryProducts={categoryProducts}
+									setCurrentSlide={setCurrentSlide}
+									setLoaded={setLoaded}
+									setDots={setDots}
+									loaded={loaded}
+									currentSlide={currentSlide}
+									dots={dots}
+								/>
+							</Show>
+						</Transition>
+
+						<Transition
+							onEnter={(el, done) => {
+								const a = el.animate([{ opacity: 0 }, { opacity: 1 }], {
+									duration: 250
+								})
+								a.finished.then(done)
+							}}
+							onExit={(el, done) => {
+								const a = el.animate([{ opacity: 1 }, { opacity: 0 }], {
+									duration: 0
+								})
+								a.finished.then(done)
+							}}
+						>
+							<Show when={categoryProducts()?.length > 10}>
+								<TripleLineSlider
+									slideVisible={4}
+									categoryProducts={categoryProducts}
+									setCurrentSlide={setCurrentSlide}
+									setLoaded={setLoaded}
+									setDots={setDots}
+									loaded={loaded}
+									currentSlide={currentSlide}
+									dots={dots}
+								/>
+							</Show>
+						</Transition>
 					</div>
 				</div>
 			</main>

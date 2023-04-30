7183809dedb1ec2d760511ccbdffa2cb7480ea47
Kilowon <shauncameron.inbox@gmail.com>
2023-04-29 20:25:44 -0400

    wip: descriptions on mouseover for each text line

--- src/Components/checkout_components/InputLabel.tsx
+++ src/Components/checkout_components/InputLabel.tsx
@@ -3,6 +3,7 @@ import { Show } from 'solid-js'
 
 type InputLabelProps = {
 	name: string
+	description?: string
 	label?: string
 	required?: boolean
 	margin?: 'none'
@@ -18,9 +19,13 @@ export function InputLabel(props: InputLabelProps) {
 				class={clsx('inline-block font-medium text-sm')}
 				for={props.name}
 			>
-				{props.label}{' '}
-				{props.required && (
-					<span class="ml-1 text-red-600 dark:text-red-400">*</span>
+				{props.label}
+				{props.required && <span class=" text-red-600 dark:text-red-400">*</span>}
+
+				{props.description && (
+					<span class="text-sm  text-slate-500/70 dark:text-slate-400 mx-9">
+						{props.description}
+					</span>
 				)}
 			</label>
 		</Show>

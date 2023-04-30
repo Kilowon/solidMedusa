107b1204f548dccaa7d278c2456f8f9e0b149853
Kilowon <shauncameron.inbox@gmail.com>
2023-04-29 20:24:35 -0400

    styling changes

--- src/Components/checkout_components/InputError.tsx
+++ src/Components/checkout_components/InputError.tsx
@@ -12,7 +12,7 @@ export function InputError(props: InputErrorProps) {
 	return (
 		<Expandable expanded={!!props.error}>
 			<div
-				class="pt-4 text-sm text-red-500 dark:text-red-400 md:text-base lg:pt-5 lg:text-lg"
+				class="pt-4 text-sm text-red-600 dark:text-red-400 md:text-base lg:pt-0 lg:text-lg bg-red-5/20 rounded-sm p-2 font-medium"
 				id={`${props.name}-error`}
 			>
 				{props.error}

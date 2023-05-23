2db137b9c00b7db414d2d1cda3bb8825bd92260a
Kilowon <shauncameron.inbox@gmail.com>
2023-05-23 00:00:15 -0400

    removed create route data

--- src/routes/(app)/products/[handle].tsx
+++ src/routes/(app)/products/[handle].tsx
@@ -1,90 +1,48 @@
 import { useGlobalContext } from '~/Context/Providers'
-import {
-	IsClientCheck,
-	fetchProduct,
-	getProductInfo
-} from '~/Services/medusaAPI'
-import { Cart } from '~/types/types'
-import {
-	createRouteData,
-	useRouteData,
-	useParams,
-	Title,
-	Meta,
-	RouteDataArgs,
-	ServerError
-} from 'solid-start'
-import { createEffect, Suspense, createSignal } from 'solid-js'
+import { fetchProduct } from '~/Services/medusaAPI'
+import { useParams, Title, Meta } from 'solid-start'
+import { Suspense } from 'solid-js'
 import ProductTemplate from '~/Components/ProductTemplate'
 import { useStore } from '~/Context/StoreContext'
 import { StoreProvider } from '~/Context/StoreContext'
+import { createQuery } from '@tanstack/solid-query'
 
-interface ProductPage {
-	products: {
-		title: string
-		description: string
-		thumbnail: string
-		images: {
-			url: string
-			id: string
-		}[]
-		id: string
-	}[]
-}
-
-export function useProduct(params: any) {
+export default function Products() {
 	const { medusa } = useGlobalContext()
 	const { cart } = useGlobalContext()
 
-	return createRouteData(
-		async () => {
-			try {
-				const productPage: ProductPage = await fetchProduct(medusa, params.handle)
-				const productInfo = await getProductInfo(
-					medusa,
-					cart,
-					productPage.products[0].id
-				)
-				return { productPage, productInfo }
-			} catch (e: any) {
-				console.log('ERROR', e.stack)
-				throw new ServerError(e.message)
-			}
-		},
-		{ key: () => params.handle }
-	)
-}
-
-export default function Products() {
 	const params = useParams()
-	const data = useProduct(params)
-	data()
-	const { setCurrentProductId } = useGlobalContext()
 
-	createEffect(() => {
-		setCurrentProductId?.(String(data()?.productPage.products[0].id))
-	})
+	const queryProduct = createQuery(() => ({
+		queryKey: ['product', params.handle],
+		queryFn: async function () {
+			const product = await fetchProduct(medusa, cart, params.handle)
+			return product
+		},
+		cacheTime: 25 * 60 * 1000,
+		staleTime: 25 * 60 * 1000
+	}))
 
 	return (
 		<div>
-			<Title>{data()?.productPage.products[0].title}</Title>
+			<Title>{queryProduct?.data?.products[0]?.title}</Title>
 			<Meta
 				itemProp="description"
-				content={data()?.productPage.products[0].description}
+				content={queryProduct?.data?.products[0]?.description}
 			/>
 			<Meta
 				itemProp="og:title"
-				content={data()?.productPage.products[0].title}
+				content={queryProduct?.data?.products[0]?.title}
 			/>
 			<Meta
 				itemProp="image"
-				content={data()?.productPage.products[0].thumbnail}
+				content={queryProduct?.data?.products[0]?.thumbnail}
 			/>
 			<main>
-				<StoreProvider product={data()?.productInfo?.product}>
+				<StoreProvider product={queryProduct?.data?.products[0]}>
 					<ProductTemplate
-						images={data()?.productPage.products[0].images}
-						productInfo={data()?.productInfo?.product}
+						images={queryProduct?.data?.products[0]?.images}
+						productInfo={queryProduct?.data?.products[0]}
 						params={params?.handle}
 						updateOptions={useStore().updateOptions}
 						options={useStore().options}

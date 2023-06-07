3343c6333b753a286a8658a02253bc3562924f1d
Kilowon <shauncameron.inbox@gmail.com>
2023-06-06 22:04:00 -0400

    rework store page

--- src/routes/(app)/store/[handle].tsx
+++ src/routes/(app)/store/[handle].tsx
@@ -1,145 +1,77 @@
 import { useGlobalContext } from '~/Context/Providers'
-import { useParams, Title, Meta } from 'solid-start'
-import { createEffect, createSignal, Show, For } from 'solid-js'
+import { createSignal, Show, For } from 'solid-js'
 import 'solid-slider/slider.css'
 import { Transition } from 'solid-transition-group'
-import { FlexCategories } from '~/Components/common/FlexCategories'
-import { SingleLineSlider, DoubleLineSlider, TripleLineSlider } from '~/Components/common/ProductSlider'
-import { Route, Routes } from 'solid-start'
-import Categories from '~/routes/(app)/categories/[handle]'
+
+import { getWindowSize } from '@solid-primitives/resize-observer'
+import { createQuery } from '@tanstack/solid-query'
+import ProductPreview from '~/Components/nav_components/ProductPreview'
 
 export default function Store() {
-	const params = useParams()
-	const { categories } = useGlobalContext()
-	const { categoryProducts } = useGlobalContext()
-	const [currentSlide, setCurrentSlide] = createSignal(0)
-	const [loaded, setLoaded] = createSignal(false)
-	const [dots, setDots] = createSignal()
-	const [parentCategories, setParentCategories] = createSignal([])
-	const { setCurrentCategoryId } = useGlobalContext()
-	function filterCategories() {
-		return categories()?.filter((category: any) => category.handle === params.handle)
-	}
+	const { medusa } = useGlobalContext()
+	const { queryCart } = useGlobalContext()
 
-	const [currentCategory, setCategory] = createSignal(filterCategories())
+	const [size, setSize] = createSignal(getWindowSize())
 
-	createEffect(() => {
-		if (categoryProducts()?.length === 0) {
-			setDots(Number(0))
+	function sidesVisible() {
+		if (size().width > 1500) {
+			return 4
 		}
-	})
-
-	function getParentCategories(categories: any[], params: any) {
-		const parentCategories = [categories.find((category: any) => category.handle === params.handle)]
-
-		while (parentCategories[parentCategories.length - 1]?.parent_category) {
-			const parentCategory = categories.find(
-				(category: any) => category.name === parentCategories[parentCategories.length - 1]?.parent_category?.name
-			)
-			parentCategories.push(parentCategory)
+		if (size().width > 900) {
+			return 3
 		}
-
-		setParentCategories(parentCategories.reverse() as never[])
-
-		return parentCategories
+		if (size().width <= 900) {
+			return 2
+		}
 	}
 
-	createEffect(() => {
-		getParentCategories(categories(), params)
-	}, [currentCategory()])
-
-	function filterCategoriesRoot() {
-		return categories()?.filter((category: any) => category.parent_category === null)
-	}
-
-	const [currentCategoryRoot, setCategoryRoot] = createSignal(filterCategoriesRoot())
+	const queryAllProducts = createQuery(() => ({
+		queryKey: ['all-products'],
+		queryFn: async function () {
+			const product = await medusa?.products?.list({
+				is_giftcard: false,
+				cart_id: queryCart?.data?.cart?.id,
+				region_id: queryCart?.data?.cart?.region_id,
+				limit: 100
+			})
+			return product
+		},
+		cacheTime: 15 * 60 * 1000,
+		staleTime: 15 * 60 * 1000
+	}))
 
 	return (
 		<main>
-			<Title>{currentCategory()[0]?.name}</Title>
-			<Meta
-				itemProp="description"
-				content={currentCategory()[0]?.handle}
-			/>
-			<Meta
-				itemProp="og:title"
-				content={currentCategory()[0]?.name}
-			/>
-			<For each={currentCategoryRoot()?.reverse()}>
-				{category => {
-					setCurrentCategoryId?.([category?.id])
-
-					return (
-						<div>
-							<div>{category.name}</div>
-							<div class="py-12 ">
-								<div class="content-container py-12 font-poppins antialiased ">
-									<FlexCategories
-										parentCategories={parentCategories}
-										currentCategory={currentCategory}
-									/>
-
-									<Transition
-										onEnter={(el, done) => {
-											const a = el.animate([{ opacity: 0 }, { opacity: 1 }], {
-												duration: 250
-											})
-											a.finished.then(done)
-										}}
-										onExit={(el, done) => {
-											const a = el.animate([{ opacity: 1 }, { opacity: 0 }], {
-												duration: 0
-											})
-											a.finished.then(done)
-										}}
-									>
-										<Show when={categoryProducts()?.length <= 6}>
-											<SingleLineSlider
-												slideVisible={3}
-												categoryProducts={categoryProducts}
-												setCurrentSlide={setCurrentSlide}
-												setLoaded={setLoaded}
-												setDots={setDots}
-												loaded={loaded}
-												currentSlide={currentSlide}
-												dots={dots}
-											/>
-										</Show>
-									</Transition>
-
-									<Transition
-										onEnter={(el, done) => {
-											const a = el.animate([{ opacity: 0 }, { opacity: 1 }], {
-												duration: 250
-											})
-											a.finished.then(done)
-										}}
-										onExit={(el, done) => {
-											const a = el.animate([{ opacity: 1 }, { opacity: 0 }], {
-												duration: 0
-											})
-											a.finished.then(done)
-										}}
-									>
-										<Show when={categoryProducts()?.length > 6}>
-											<DoubleLineSlider
-												slideVisible={5}
-												categoryProducts={categoryProducts}
-												setCurrentSlide={setCurrentSlide}
-												setLoaded={setLoaded}
-												setDots={setDots}
-												loaded={loaded}
-												currentSlide={currentSlide}
-												dots={dots}
-											/>
-										</Show>
-									</Transition>
-								</div>
-							</div>
-						</div>
-					)
+			<Transition
+				onEnter={(el, done) => {
+					const a = el.animate([{ opacity: 0 }, { opacity: 1 }], {
+						duration: 250
+					})
+					a.finished.then(done)
 				}}
-			</For>
+				onExit={(el, done) => {
+					const a = el.animate([{ opacity: 1 }, { opacity: 0 }], {
+						duration: 0
+					})
+					a.finished.then(done)
+				}}
+			>
+				<div class="content-container">
+					<Show when={queryAllProducts?.data?.products}>
+						<ol class="grid grid-cols-12 gap-4">
+							<For each={queryAllProducts?.data?.products}>
+								{product => {
+									return (
+										<li class="col-span-6 md:col-span-4 lg:col-span-3 ">
+											<ProductPreview {...product} />
+										</li>
+									)
+								}}
+							</For>
+						</ol>
+					</Show>
+				</div>
+			</Transition>
 		</main>
 	)
 }

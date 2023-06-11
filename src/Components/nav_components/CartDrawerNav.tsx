be6938207ba0e4a061c288e6b0c9ef327484595e
Kilowon <shauncameron.inbox@gmail.com>
2023-06-11 11:21:45 -0400

    show for cart

--- src/Components/nav_components/CartDrawerNav.tsx
+++ src/Components/nav_components/CartDrawerNav.tsx
@@ -1,4 +1,4 @@
-import { createEffect, createSignal, lazy, ErrorBoundary } from 'solid-js'
+import { createEffect, createSignal, lazy, ErrorBoundary, Show } from 'solid-js'
 import { useGlobalContext } from '~/Context/Providers'
 import { isServer } from 'solid-js/web'
 
@@ -23,43 +23,45 @@ export default function CartDrawerNav(props: any) {
 	})
 
 	return (
-		<ErrorBoundary fallback={<div>f</div>}>
-			<div>
-				<div
-					class="flex items-center rounded-full xl:hidden z-1 relative"
-					style="position: fixed; top: 0.85vh; right: -0.5rem; width: 3.75rem; height: 3rem;"
-					title="Cart"
-					role="button"
-					tabindex="0"
-					onClick={() => props.setCartDrawer({ cart: 'active', checkout: 'active' })}
-				>
-					<div class="i-ion-cart-outline h-7 w-7 absolute top-3 left-1.75" />
-					<div class="absolute top-1.5 left-5.8 w-4 h-4 text-white text-xs flex items-center justify-center bg-gray-5 rounded-full">
-						{totalItemsInCart(items())}
-					</div>
-				</div>
-				<div
-					class={`fixed inset-0 bg-white/30 z-200 transition-all duration-250 ease-in-out ${
-						props.cartDrawer().cart === 'active' ? '' : 'opacity-0 pointer-events-none'
-					}`}
-					onClick={event => {
-						if (event.target === event.currentTarget) {
-							props.setCartDrawer({ cart: 'hidden', checkout: 'active' })
-						}
-					}}
-				>
-					<div
-						class={`fixed top-12 right-0 h-full w-[95vw] sm:w-[85vw] md:w-[45vw] bg-white z-200 transform rounded-sm  transition-transform duration-500 ease-in-out p-2 ${
-							props.cartDrawer().cart === 'active' ? '' : 'translate-x-full'
-						}`}
-					>
-						<CartCore
-							variant="mobile-panel"
-							setCartDrawer={props.setCartDrawer()}
-						/>
-					</div>
+		<div>
+			<div
+				class="flex items-center rounded-full xl:hidden z-1 relative"
+				style="position: fixed; top: 0.85vh; right: -0.5rem; width: 3.75rem; height: 3rem;"
+				title="Cart"
+				role="button"
+				tabindex="0"
+				onClick={() => props.setCartDrawer({ cart: 'active', checkout: 'active' })}
+			>
+				<div class="i-ion-cart-outline h-7 w-7 absolute top-3 left-1.75" />
+				<div class="absolute top-1.5 left-5.8 w-4 h-4 text-white text-xs flex items-center justify-center bg-gray-5 rounded-full">
+					{totalItemsInCart(items())}
 				</div>
 			</div>
-		</ErrorBoundary>
+			<div
+				class={`fixed inset-0 bg-white/30 z-200 transition-all duration-250 ease-in-out ${
+					props.cartDrawer().cart === 'active' ? '' : 'opacity-0 pointer-events-none'
+				}`}
+				onClick={event => {
+					if (event.target === event.currentTarget) {
+						props.setCartDrawer({ cart: 'hidden', checkout: 'active' })
+					}
+				}}
+			>
+				<div
+					class={`fixed top-12 right-0 h-full w-[95vw] sm:w-[85vw] md:w-[45vw] bg-white z-200 transform rounded-sm  transition-transform duration-500 ease-in-out p-2 ${
+						props.cartDrawer().cart === 'active' ? '' : 'translate-x-full'
+					}`}
+				>
+					<ErrorBoundary fallback={<div>f</div>}>
+						<Show when={!isServer && props.cartDrawer().cart === 'active'}>
+							<CartCore
+								variant="mobile-panel"
+								setCartDrawer={props.setCartDrawer()}
+							/>
+						</Show>
+					</ErrorBoundary>
+				</div>
+			</div>
+		</div>
 	)
 }

b80cf26599cc7d777da263f0b081c96f6fe1062d
Kilowon <shauncameron.inbox@gmail.com>
2023-06-12 18:09:46 -0400

    testing perf

--- src/Components/layout/Hero.tsx
+++ src/Components/layout/Hero.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'solid-js'
 import { A } from 'solid-start'
 import { Image } from '@unpic/solid'
+import { Transition } from 'solid-transition-group'
 
 export function Hero() {
 	return (
@@ -8,9 +9,17 @@ export function Hero() {
 			class="h-[100svh] w-full top-0 left-0 z-0 bg-cover bg-center bg-no-repeat 
 		"
 		>
-			<div class="text-white absolute inset-0 z-10 flex flex-col  items-center text-center sm:text-left md:justify-end md:items-start md:p-32 font-poppins">
-				<Suspense>
-					<span>
+			<Transition
+				appear={true}
+				onEnter={(el, done) => {
+					const a = el.animate([{ opacity: 0 }, { opacity: 1 }], {
+						duration: 400
+					})
+					a.finished.then(done)
+				}}
+			>
+				<div class="text-white absolute inset-0 z-10 flex flex-col  items-center text-center sm:text-left md:justify-end md:items-start md:p-32 font-poppins">
+					{/* <Suspense>
 						<div class="relative block md:hidden mt-[20vh]">
 							<Image
 								src="https://res.cloudinary.com/contentdelivery/image/upload/v1684413389/couch_npht3q.webp"
@@ -19,28 +28,28 @@ export function Hero() {
 								alt="couch"
 							/>
 							<div class="absolute text-white top-6 right-3.5 z-15 text-2xl font-bold font-poppins ">ME</div>
-							{/* <h1 class=" absolute top-17 left--17 w-300% text-xl md:text-2xl drop-shadow-md shadow-black flex items-baseline font-poppins">
-							Embrace Modern Design
-						</h1> */}
+							<h1 class=" absolute top-17 left--17 w-300% text-xl md:text-2xl drop-shadow-md shadow-black flex items-baseline font-poppins">
+								Embrace Modern Design
+							</h1>
 						</div>
-					</span>
-				</Suspense>
+					</Suspense> */}
 
-				<h1 class="hidden  text-xl md:text-6xl lg:text-7xl xl:text-9xl mb-4 drop-shadow-md shadow-black md:flex items-baseline font-poppins z-2">
-					Embrace Modern Design
-				</h1>
-				<div class="flex items-center hover:underline">
-					<A
-						href="/store/Store"
-						class="text-xl md:text-4xl z-2"
-					>
-						Explore products
-					</A>
-					<div class="text-xl md:text-5xl">
-						<div class="i-material-symbols-arrow-right-alt-rounded" />
+					<h1 class="hidden  text-xl md:text-6xl lg:text-7xl xl:text-9xl mb-4 drop-shadow-md shadow-black md:flex items-baseline font-poppins z-2">
+						Embrace Modern Design
+					</h1>
+					<div class="flex items-center hover:underline">
+						<A
+							href="/store/Store"
+							class="text-xl md:text-4xl z-2"
+						>
+							Explore products
+						</A>
+						<div class="text-xl md:text-5xl">
+							<div class="i-material-symbols-arrow-right-alt-rounded" />
+						</div>
 					</div>
 				</div>
-			</div>
+			</Transition>
 
 			<Image
 				src="https://res.cloudinary.com/contentdelivery/image/upload/v1682174583/martin-pechy-bpg-ngqrPc8-unsplash_ctefbx.jpg"

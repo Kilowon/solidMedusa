0840f0a4ac0e98c4931e52891cfecb6ce4aebc90
Kilowon <shauncameron.inbox@gmail.com>
2023-06-16 14:24:42 -0400

    remove transition to test pref

--- src/Components/layout/Hero.tsx
+++ src/Components/layout/Hero.tsx
@@ -9,18 +9,9 @@ export function Hero() {
 			class="h-[100svh] w-full top-0 left-0 z-0 bg-cover bg-center bg-no-repeat 
 		"
 		>
-			<Transition
-				appear={true}
-				onEnter={(el, done) => {
-					const a = el.animate([{ opacity: 0 }, { opacity: 1 }], {
-						duration: 3000
-					})
-					a.finished.then(done)
-				}}
-			>
-				<div class="text-white absolute inset-0 z-10 flex flex-col  items-center text-center sm:text-left md:justify-end md:items-start md:p-32 font-poppins">
-					<Suspense>
-						{/* 
+			<div class="text-white absolute inset-0 z-10 flex flex-col  items-center text-center sm:text-left md:justify-end md:items-start md:p-32 font-poppins">
+				<Suspense>
+					{/* 
 						<div class="relative block md:hidden mt-[20vh]">
 							<Image
 								src="https://res.cloudinary.com/contentdelivery/image/upload/v1684413389/couch_npht3q.webp"
@@ -34,24 +25,23 @@ export function Hero() {
 							</h1>
 						</div>
 					*/}
-					</Suspense>
+				</Suspense>
 
-					<h1 class="hidden  text-xl md:text-6xl lg:text-7xl xl:text-9xl mb-4 drop-shadow-md shadow-black md:flex items-baseline font-poppins font-400 z-2">
-						Embrace Modern Design
-					</h1>
-					<div class="flex items-center hover:underline">
-						<A
-							href="/store/Store"
-							class="text-xl md:text-4xl z-2"
-						>
-							Explore products
-						</A>
-						<div class="text-xl md:text-5xl">
-							<div class="i-material-symbols-arrow-right-alt-rounded" />
-						</div>
+				<h1 class="hidden  text-xl md:text-6xl lg:text-7xl xl:text-9xl mb-4 drop-shadow-md shadow-black md:flex items-baseline font-poppins font-400 z-2">
+					Embrace Modern Design
+				</h1>
+				<div class="flex items-center hover:underline">
+					<A
+						href="/store/Store"
+						class="text-xl md:text-4xl z-2"
+					>
+						Explore products
+					</A>
+					<div class="text-xl md:text-5xl">
+						<div class="i-material-symbols-arrow-right-alt-rounded" />
 					</div>
 				</div>
-			</Transition>
+			</div>
 
 			<Image
 				src="https://res.cloudinary.com/contentdelivery/image/upload/v1682174583/martin-pechy-bpg-ngqrPc8-unsplash_ctefbx.jpg"

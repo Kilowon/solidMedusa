8215bd08c8a2c5bb27ec9c29590c3df0a8439549
Kilowon <shauncameron.inbox@gmail.com>
2023-07-01 22:28:45 -0400

    200 count

--- src/Context/Providers.tsx
+++ src/Context/Providers.tsx
@@ -163,7 +163,7 @@ export function GlobalContextProvider(props: any) {
 	const queryCategories = createQuery(() => ({
 		queryKey: ['categories_list'],
 		queryFn: async function () {
-			const product = await medusa.productCategories.list({})
+			const product = await medusa.productCategories.list({ limit: '200' })
 			return product
 		},
 		cacheTime: 15 * 60 * 1000

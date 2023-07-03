dc2879a252bce7e51f3e364f2f8600bccfb09a82
Kilowon <shauncameron.inbox@gmail.com>
2023-07-03 09:06:30 -0400

    Finalized carousel

--- src/Components/layout/Hero.tsx
+++ src/Components/layout/Hero.tsx
@@ -1,10 +1,11 @@
-import { Show, For, createEffect, createSignal, createMemo, onCleanup } from 'solid-js'
+import { Show, For, createEffect, createSignal, createMemo, onCleanup, onMount } from 'solid-js'
 import { A } from 'solid-start'
 import { Image } from '@unpic/solid'
 import { getWindowSize } from '@solid-primitives/resize-observer'
 import { useGlobalContext } from '~/Context/Providers'
 import { Motion, Presence } from '@motionone/solid'
 import { Rerun } from '@solid-primitives/keyed'
+import clsx from 'clsx'
 
 export function Hero() {
 	let heroArray = [
@@ -34,45 +35,40 @@ export function Hero() {
 
 	const { rootCategories } = useGlobalContext()
 
-	const [currentIndex, setCurrentIndex] = createSignal(0)
-	const [count, setCount] = createSignal(1)
-	const [imageIndex, setImageIndex] = createSignal(0)
-	const [imageCount, setImageCount] = createSignal(0)
+	const [currentIndex, setCurrentIndex] = createSignal(heroArray.length - 1)
+	const [count, setCount] = createSignal(heroArray.length - 1)
 	const [time, setTime] = createSignal(5000)
 	const [endWait, setEndWait] = createSignal(15000)
+	const [colorWheel, setColorWheel] = createSignal('maincolor')
 
-	createEffect(() => {
-		const timeout = setTimeout(() => {
-			const interval = setInterval(() => {
+	function heroCarousel() {
+		let interval: any
+		const startInterval = () => {
+			interval = setInterval(() => {
+				console.log(colorWheel(), currentIndex(), count())
 				if (currentIndex() === heroArray.length - 1) {
+					setColorWheel('herocolor')
+					clearInterval(interval) // clear the interval
 					setTimeout(() => {
-						setCount(p => ++p)
-						setCurrentIndex((currentIndex() + 1) % heroArray.length)
+						setCount(0)
+						setCurrentIndex(0)
+
+						startInterval() // restart the interval
 					}, endWait())
 				}
-				if (currentIndex() !== heroArray.length - 1) {
+				if (currentIndex() < heroArray.length - 1) {
+					setColorWheel('maincolor')
 					setCount(p => ++p)
 					setCurrentIndex((currentIndex() + 1) % heroArray.length)
 				}
 			}, time())
-		}, 2000)
+		}
+		startInterval()
+	}
 
-		onCleanup(() => clearTimeout(timeout))
-	})
-	createEffect(() => {
+	onMount(() => {
 		const timeout = setTimeout(() => {
-			const interval = setInterval(() => {
-				if (imageIndex() === heroImages.length - 1) {
-					setTimeout(() => {
-						setImageCount(p => ++p)
-						setImageIndex((imageIndex() + 1) % heroImages.length)
-					}, endWait())
-				}
-				if (imageIndex() !== heroImages.length - 1) {
-					setImageCount(p => ++p)
-					setImageIndex((imageIndex() + 1) % heroImages.length)
-				}
-			}, time())
+			heroCarousel()
 		}, 2000)
 
 		onCleanup(() => clearTimeout(timeout))
@@ -80,9 +76,10 @@ export function Hero() {
 
 	return (
 		<div
-			class="h-[100svh] w-full top-0 left-0 z-0 bg-cover bg-center bg-no-repeat flex items-center md:items-end
+			class="h-[100svh] w-full top-0 left-0 z--20 bg-cover bg-center bg-no-repeat flex items-center md:items-end 
 		"
 		>
+			<div class="absolute bg-black w-[100svw] h-[100svh] z--40" />
 			<Presence initial>
 				<Show when={rootCategories()}>
 					<div class="text-white z-10 flex flex-col items-start mb-20 md:ml-10">
@@ -204,7 +201,7 @@ export function Hero() {
 												animate={{ opacity: [1, 0, 1], y: [-300, 0], transition: { duration: 1.75 } }}
 												exit={{ opacity: 0, y: [0, 150], transition: { duration: 0.5, easing: 'ease-out' } }}
 											>
-												<h1 class="tracking-tighter text-6xl md:text-6xl lg:text-7xl xl:text-9xl mb-4 drop-shadow-md shadow-black font-700 text-blue-3 z-2 absolute">{`${
+												<h1 class="tracking-tighter text-6xl md:text-6xl lg:text-7xl xl:text-9xl mb-4 text-blue-3 drop-shadow-md shadow-black font-700 z-2">{`${
 													heroArray[currentIndex()]
 												}`}</h1>
 											</Motion.div>
@@ -247,27 +244,43 @@ export function Hero() {
 								</div>
 							</div>
 						</Motion.div>
+						<div class="absolute bottom-4 left-4 flex space-x-2">
+							<For each={heroArray}>
+								{(item, index) => {
+									return (
+										<div>
+											<Show when={index() === currentIndex()}>
+												<div class="w-5 h-2 rounded-6 bg-blue-3" />
+											</Show>
+											<Show when={index() !== currentIndex()}>
+												<div class="w-5 h-2 rounded-6 bg-gray-3/30" />
+											</Show>
+										</div>
+									)
+								}}
+							</For>
+						</div>
 					</div>
 				</Show>
 			</Presence>
 			<Show when={getWindowSize().width > 768}>
 				<div class="hidden">
 					<Image
-						src={heroImages[(imageIndex() + 1) % heroImages.length]}
+						src={heroImages[(currentIndex() + 1) % heroImages.length]}
 						layout="fullWidth"
 						priority={true}
-						class="object-cover object-right md:object-center h-full w-full z--10 absolute inset-0  filter brightness-65"
+						class="object-cover object-right md:object-center h-full w-full z--10 absolute inset-0  filter brightness-65 "
 						alt="Photo by @thevoncomplex https://unsplash.com/@thevoncomplex"
 					/>
 				</div>
 				<Presence>
-					<Rerun on={imageCount()}>
+					<Rerun on={count()}>
 						<Motion.div
 							animate={{ opacity: [0.3, 1], transition: { duration: 1.0 } }}
 							exit={{ opacity: 0, transition: { duration: 0.5 } }}
 						>
 							<Image
-								src={heroImages[imageIndex()]}
+								src={heroImages[currentIndex()]}
 								layout="fullWidth"
 								priority={true}
 								class="object-cover object-right md:object-center h-full w-full z--10 absolute inset-0  filter brightness-65"
@@ -280,7 +293,7 @@ export function Hero() {
 			<Show when={getWindowSize().width < 768}>
 				<div class="hidden">
 					<Image
-						src={heroImages[(imageIndex() + 1) % heroImages.length]}
+						src={heroImages[(currentIndex() + 1) % heroImages.length]}
 						layout="fullWidth"
 						priority={true}
 						class="object-cover object-right md:object-center h-full w-full z--10 absolute inset-0  filter brightness-65"
@@ -288,13 +301,13 @@ export function Hero() {
 					/>
 				</div>
 				<Presence>
-					<Rerun on={imageCount()}>
+					<Rerun on={count()}>
 						<Motion.div
 							animate={{ opacity: [0.3, 1], transition: { duration: 1.0 } }}
 							exit={{ opacity: 0, transition: { duration: 0.5 } }}
 						>
 							<Image
-								src={heroImages[imageIndex()]}
+								src={heroImages[currentIndex()]}
 								layout="fullWidth"
 								priority={true}
 								class="object-cover object-right md:object-center h-full w-full z--10 absolute inset-0  filter brightness-65"

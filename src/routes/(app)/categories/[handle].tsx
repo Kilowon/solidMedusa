74d49e29c97ae572406de2efed835b844908ae60
Kilowon <shauncameron.inbox@gmail.com>
2023-07-06 12:11:38 -0400

    cont

--- src/routes/(app)/categories/[handle].tsx
+++ src/routes/(app)/categories/[handle].tsx
@@ -119,7 +119,7 @@ export default function Categories() {
 									/>
 
 									<Show
-										when={queryCategoryProducts.data?.products?.length > 0}
+										when={queryCategoryProducts.isSuccess}
 										fallback={<div class="w-[100px] h-[275px] bg-purple-8"></div>}
 									>
 										<ul class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-2">

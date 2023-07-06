8555e834ee40cd45b38563084957cf09272c7107
Kilowon <shauncameron.inbox@gmail.com>
2023-07-06 17:58:25 -0400

    adjust text size

--- src/Components/common/FlexCategories.tsx
+++ src/Components/common/FlexCategories.tsx
@@ -5,7 +5,7 @@ import { TransitionGroup } from 'solid-transition-group'
 export function FlexCategories(props: any) {
 	return (
 		<div class="flex flex-col  mb-16 ">
-			<span class="text-sm md:text-base text-gray-600 mb-6">Shop By Category</span>
+			<span class="text-xs md:text-base text-gray-600 mb-6">Shop By Category</span>
 			<div class=" flex flex-row text-sm lg:text-2xl text-gray-900 ">
 				<ol class="flex flex-row">
 					<Show when={props.parentCategories()}>
@@ -25,11 +25,11 @@ export function FlexCategories(props: any) {
 						>
 							<For each={props.parentCategories()}>
 								{(category: any) => (
-									<li class="flex flex-row whitespace-nowrap space-x-0.5">
+									<li class="flex flex-row whitespace-nowrap space-x-0.5 text-xs lg:text-base flex items-center text-gray-8">
 										<A href={`/categories/${category?.handle}`}>
 											<div class={category?.name === props.currentCategory?.()[0]?.name ? 'flex' : ''}>{category?.name}</div>
 										</A>
-										<div class={' font-bold lg:text-2xl '}>
+										<div class={' font-bold lg:text-xl '}>
 											<div class={category?.category_children?.length === 0 ? '' : 'i-ic-outline-chevron-right'} />
 										</div>
 									</li>
@@ -58,7 +58,7 @@ export function FlexCategories(props: any) {
 							<For each={props.currentCategory()[0]?.category_children}>
 								{(child: any, index) => (
 									<div class="flex flex-row whitespace-nowrap items-center justify-center">
-										<li class="text-sm lg:text-base border border-gray-5 text-gray-5 px-0.5  lg:px-1 lg:h-6  rounded-2  flex ">
+										<li class="text-xs lg:text-base border border-gray-5 text-gray-5 px-0.5  lg:px-1 lg:h-6  rounded-2  flex ">
 											<A href={`/categories/${child?.handle}`}>{child?.name}</A>
 										</li>
 										<div class="flex flex-row items-center text-xl pr-1   "></div>

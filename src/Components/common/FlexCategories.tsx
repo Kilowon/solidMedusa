abb62501eec5162cf80d7f16b71fc7bea1a75dff
Kilowon <shauncameron.inbox@gmail.com>
2023-07-06 18:13:08 -0400

    keep flex for large width and grid for small

--- src/Components/common/FlexCategories.tsx
+++ src/Components/common/FlexCategories.tsx
@@ -39,7 +39,7 @@ export function FlexCategories(props: any) {
 					</Show>
 				</ol>
 
-				<ol class="grid grid-cols-2 md:grid-cols-4 gap-1.5">
+				<ol class="grid grid-cols-2 gap-1.5 md:flex md:flex-row md:flex-wrap">
 					<Show when={props.currentCategory?.()[0]?.category_children}>
 						<TransitionGroup
 							onEnter={(el, done) => {

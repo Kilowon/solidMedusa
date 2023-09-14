893e4c7380c0f7fc187f13a78409163f5f0e1fbd
Kilowon <shauncameron.inbox@gmail.com>
2023-09-14 18:45:26 -0400

    express checkout work

--- src/Components/checkout_components/Express.tsx
+++ src/Components/checkout_components/Express.tsx
@@ -2,12 +2,42 @@ import { Show, createSignal, onMount, createEffect } from 'solid-js'
 import { loadScript } from '@paypal/paypal-js'
 import { getWindowSize } from '@solid-primitives/resize-observer'
 import ExpressShipping from '~/Components/checkout_components/ExpressShipping'
+import { createQuery } from '@tanstack/solid-query'
+import { useGlobalContext } from '~/Context/Providers'
+import { loadStripe } from '@stripe/stripe-js'
+import type { PaymentRequestPaymentMethodEvent } from '@stripe/stripe-js'
+import {
+	Elements,
+	useElements,
+	useStripe,
+	LinkAuthenticationElement,
+	PaymentElement,
+	PaymentRequestButton
+} from 'solid-stripe'
+import { useNavigate } from 'solid-start'
 
 export default function Express() {
 	let paypalButtons: any
 	let paypalButtonRef: HTMLElement
 	const [windowSize, setWindowSize] = createSignal(getWindowSize())
 	const [paypalLoaded, setPaypalLoaded] = createSignal(false)
+
+	const { medusa } = useGlobalContext()
+	const { queryCart } = useGlobalContext()
+	const [stripe, setStripe] = createSignal<any>(null)
+
+	onMount(async () => {
+		const result = await loadStripe(import.meta.env.VITE_PUBLIC_STRIPE_KEY)
+		setStripe(result)
+	})
+
+	// Trigger Medusa payment session and submit payment
+	const paymentSessionQuery = createQuery(() => ({
+		queryKey: ['cart'],
+		queryFn: () => medusa?.carts.createPaymentSessions(queryCart.data?.cart?.id),
+		enabled: true,
+		cacheTime: 3 * 60 * 1000
+	}))
 
 	onMount(async () => {
 		setWindowSize(getWindowSize())
@@ -61,6 +91,95 @@ export default function Express() {
 					ref={el => (paypalButtonRef = el)}
 				></div>
 			</Show>
+			<Show when={true}>
+				<Elements
+					stripe={stripe()}
+					clientSecret={paymentSessionQuery?.data?.cart?.payment_session?.data?.client_secret}
+				>
+					<CheckoutButtons clientSecret={paymentSessionQuery?.data?.cart?.payment_session?.data?.client_secret} />
+				</Elements>
+			</Show>
 		</div>
+	)
+}
+
+export function CheckoutButtons(props: { clientSecret: string }) {
+	const stripe = useStripe()
+	const { queryCart } = useGlobalContext()
+	console.log(import.meta.env.VITE_APP_URL, 'env')
+	// Declare payment metadata (amounts must match payment intent)
+	const paymentRequest = {
+		country: 'US',
+		currency: 'usd',
+		total: { label: 'Demo total', amount: queryCart?.data?.cart?.total },
+		requestPayerName: true,
+		requestPayerEmail: true,
+		requestPayerPhone: true,
+		requestShipping: true,
+		returnURL: `${import.meta.env.VITE_APP_URL}/checkout/success`,
+		displayItems: [
+			{
+				label: 'Subtotal',
+				amount: queryCart?.data?.cart?.subtotal
+			},
+			{
+				label: 'Shipping',
+				amount: queryCart?.data?.cart?.shipping_total
+			},
+			{
+				label: 'Tax',
+				amount: queryCart?.data?.cart?.tax_total
+			},
+			{
+				label: 'Total',
+				amount: queryCart?.data?.cart?.total
+			}
+		],
+		shippingOptions: [
+			{
+				id: 'free-shipping',
+				label: 'Free shipping',
+				detail: 'Arrives in 5 to 7 days',
+				amount: 0
+
+				// selected: true
+			},
+			{
+				id: 'express-shipping',
+				label: 'Express shipping',
+				detail: 'Arrives in 1 to 3 days',
+				amount: 599
+
+				// selected: false
+			}
+		]
+	}
+
+	async function handlePaymentRequest(ev: PaymentRequestPaymentMethodEvent) {
+		// Create payment intent server side
+
+		const result = await stripe()?.confirmCardPayment(props.clientSecret, {
+			payment_method: ev.paymentMethod.id
+		})
+
+		if (result?.error) {
+			// Report to the browser that the payment failed,
+			// prompting it to re-show the payment interface,
+			// or show an error message and close the payment.
+			ev.complete('fail')
+		} else {
+			// Report to the browser that the confirmation was
+			// successful, prompting it to close the browser
+			// payment method collection interface.
+			ev.complete('success')
+		}
+	}
+
+	return (
+		<PaymentRequestButton
+			paymentRequest={paymentRequest}
+			onPaymentMethod={handlePaymentRequest}
+			style={{ paymentRequestButton: { theme: 'light', height: '42px', type: 'check-out', buttonSpacing: 'horizontal' } }}
+		/>
 	)
 }

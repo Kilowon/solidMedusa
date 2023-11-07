ecd4a7dbaf65f97b1aa3baf8f7d9302ae07e9a8d
Kilowon <shaun.inbox@skiff.com>
2023-11-07 08:07:17 -0500

    show on title

--- src/routes/(app)/collections/[handle].tsx
+++ src/routes/(app)/collections/[handle].tsx
@@ -124,32 +124,34 @@ export default function Collection() {
 				<div class="mx-1 sm:mx-auto sm:content-container ">
 					<Show when={queryCollection?.data?.products && primaryData.isSuccess}>
 						<ol class="row-start-1 col-start-1 col-span-5 grid grid-cols-2 sm:grid-cols-4 xl:grid-cols-5 gap-x-1  sm:gap-x-4 gap-y-2 sm:gap-y-16 sm:my-20">
-							<li class="col-span-1 sm:col-span-1 grid justify-self-center content-center space-y-4 text-balance">
-								<div class="space-y-1">
-									<Show when={primaryDataFeatured()?.item?.top_line_tag?.length > 0}>
-										<ul class="flex space-x-2">
-											<For each={primaryDataFeatured()?.item?.top_line_tag}>
-												{(item: any) => {
-													return (
-														<li class="flex items-center justify-center text-xs  xl:text-base font-500 px-1.5 text-normal_1 bg-accent_6 tracking-tighter text-balance rounded-0.5">
-															{item}
-														</li>
-													)
-												}}
-											</For>
-										</ul>
-									</Show>
-									<h1 class=" text-sm  xl:text-[1.2svw] font-500  text-text_1 tracking-tighter text-balance">
-										{primaryDataFeatured()?.item?.title}
-									</h1>
-									<h2 class=" text-xs   xl:text-sm font-500  text-text_2 tracking-tighter text-balance">
-										{primaryDataFeatured()?.item?.sub_title_bottom}
-									</h2>
-								</div>
-								<p class="text-xs xl:text-sm  text-text_3  tracking-normal text-balance">
-									{primaryDataFeatured()?.item?.description}
-								</p>
-							</li>
+							<Show when={primaryDataFeatured()?.item?.title}>
+								<li class="col-span-1 sm:col-span-1 grid justify-self-center content-center space-y-4 text-balance">
+									<div class="space-y-1">
+										<Show when={primaryDataFeatured()?.item?.top_line_tag?.length > 0}>
+											<ul class="flex space-x-2">
+												<For each={primaryDataFeatured()?.item?.top_line_tag}>
+													{(item: any) => {
+														return (
+															<li class="flex items-center justify-center text-xs  xl:text-base font-500 px-1.5 text-normal_1 bg-accent_6 tracking-tighter text-balance rounded-0.5">
+																{item}
+															</li>
+														)
+													}}
+												</For>
+											</ul>
+										</Show>
+										<h1 class=" text-sm  xl:text-[1.2svw] font-500  text-text_1 tracking-tighter text-balance">
+											{primaryDataFeatured()?.item?.title}
+										</h1>
+										<h2 class=" text-xs   xl:text-sm font-500  text-text_2 tracking-tighter text-balance">
+											{primaryDataFeatured()?.item?.sub_title_bottom}
+										</h2>
+									</div>
+									<p class="text-xs xl:text-sm  text-text_3  tracking-normal text-balance">
+										{primaryDataFeatured()?.item?.description}
+									</p>
+								</li>
+							</Show>
 							<For each={queryCollection?.data?.products}>
 								{(product, index) => {
 									let el: HTMLLIElement | undefined

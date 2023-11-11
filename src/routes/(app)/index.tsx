87bcbb56f559a20f8ae50200f76cca805c9b462a
Kilowon <shaun.inbox@skiff.com>
2023-11-11 10:28:25 -0500

    exp

--- src/routes/(app)/index.tsx
+++ src/routes/(app)/index.tsx
@@ -89,7 +89,7 @@ export default function App() {
 	})
 
 	return (
-		<div class="min-h-300vh">
+		<div>
 			<Suspense>
 				<HeroSection />
 
@@ -100,7 +100,7 @@ export default function App() {
 					class="w-90% h-10 bg-transparent"
 				></div>
 			</Suspense>
-			<main class="min-h-50 ">
+			<main class="min-h-800vh ">
 				<div>
 					{/* THIS IS A SAFE AREA TO ADD CUSTOM COMPONENTS ... IF you need to modify a component the best practice is to make a copy and modify that */}
 				</div>
